2d129305c1b54471016ea66eb5f9845614bf2256
Racky <racky.liu@moca-tech.net>
2019-09-16 18:43:25 +0800

    test

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import axios  from 'axios'; 
+import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap3';
+import {
+  Scheduler,
+  MonthView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
 
-export default function Index(){
-  return (
-    <div>
-      我是首页
-    </div>
-  )
+import {data} from './data';
+
+
+export default class Index extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: data,
+      currentDate: '2018-07-17',
+    };
+  }
+  // useEffect(()=>{
+  //   axios.get('http://192.168.31.180:8080/devices')
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .finally(function () {
+  //       // always executed
+  //     });
+  // },[])
+  render(){
+    const { data, currentDate } = this.state;
+    return (
+      <div>
+       <Scheduler
+          data={data}
+        >
+          <ViewState
+            currentDate={currentDate}
+          />
+          <MonthView />
+          <Appointments />
+        </Scheduler>
+      </div>
+    )
+  }
 }
---
+++ src/pages/data.js
@@ -0,0 +1,6 @@
+export const data=[
+  { startDate: '2018-07-31 10:00', endDate: '2018-07-31 11:00', title: 'Meeting' },
+  { startDate: '2018-07-30 10:00', endDate: '2018-07-30 11:00', title: 'sdsd' },
+  { startDate: '2018-07-02 18:00', endDate: '2018-07-04 19:30', title: 'Go to a gym' },
+  { startDate: '2018-07-01 18:00', endDate: '2018-07-04 19:30', title: 'Gaddssds' },
+]
